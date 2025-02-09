3a29281207cdd9445e1a401de43a3be546ae236e
wtan0144 <wtan0144@student.monash.edu>
2025-02-09 17:51:31 +1100

    Added list of tags and autocomplete interaction, also added complete button without interaction yet

--- commands/utility/random.js
+++ commands/utility/random.js
@@ -1,34 +1,84 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 
 const minPossibleRating = 800;
 const maxPossibleRating = 4000;
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('random')
-        .setDescription('Fetches a random problem from Codeforces!')
-        .addIntegerOption(option =>
-            option.setName("min_rating")
-                .setDescription('Minimum problem rating')
-                .setMinValue(minPossibleRating)
-                .setMaxValue(maxPossibleRating)
-        )
-        .addIntegerOption(option =>
-            option.setName("max_rating")
-                .setDescription('Maximum problem rating')
-                .setMinValue(minPossibleRating)
-                .setMaxValue(maxPossibleRating)
-        )
-        .addStringOption(option =>
-            option.setName("tags")
-                .setDescription('Problems that have this tag')
-                .addChoices(
-                    {name : 'dp',  value: 'dp'},
-                    {name : 'binary search',  value: 'binary search'},
-                )
-            )
+const tags = [
+    'divide and conquer',
+    'dp',
+    'flows',
+    'graphs',
+    'graph matchings',
+    'constructive algorithms',
+    'interactive',
+    'data structures',
+    'geometry',
+    'matrices',
+    'probabilities',
+    'string suffix structures',
+    'bitmasks',
+    'brute force',
+    'greedy',
+    'implementation',
+    'math',
+    '2-sat',
+    'combinatorics',
+    'sortings',
+    'expression parsing',
+    'dfs and similar',
+    'dsu',
+    'games',
+    'two pointers',
+    'binary search',
+    'shortest paths',
+    'hashing',
+    'strings',
+    'number theory',
+    'trees',
+    'schedules',
+    'fft',
+    'ternary search',
+    'chinese remainder theorem',
+    '*special',
+    'meet-in-the-middle'
+]
 
-    ,
+const random = new SlashCommandBuilder()
+    .setName('random')
+    .setDescription('Fetches a random problem from Codeforces!')
+    .addIntegerOption(option =>
+        option.setName("min_rating")
+            .setDescription('Minimum problem rating')
+            .setMinValue(minPossibleRating)
+            .setMaxValue(maxPossibleRating)
+    )
+    .addIntegerOption(option =>
+        option.setName("max_rating")
+            .setDescription('Maximum problem rating')
+            .setMinValue(minPossibleRating)
+            .setMaxValue(maxPossibleRating)
+    )
+    .addStringOption(option =>
+        option.setName("tags")
+            .setDescription('Problems that have this tag')
+            .setAutocomplete(true)
+    )
+
+//tags.map(tag => random.addBooleanOption(option => option.setName(tag).setDescription('Problems that have this tag')))
+
+module.exports = {
+    data: random,
+    async autocomplete(interaction) {
+        const focusedValue = interaction.options.getFocused();
+        const filtered = tags.filter(choice => choice.startsWith(focusedValue));
+        await interaction.respond(
+            filtered.map(choice => ({
+                name: choice,
+                value: choice
+            }))
+        )
+
+    },
     async execute(interaction) {
         await interaction.deferReply(); // Acknowledge the command to allow more time for processing
 
@@ -45,7 +95,11 @@ module.exports = {
             // define range and filter problems
             const minRating = interaction.options.getInteger('min_rating') ?? minPossibleRating;
             const maxRating = interaction.options.getInteger('max_rating') ?? maxPossibleRating;
-            const filteredProblems = problems.filter(problem => minRating <= problem.rating && problem.rating <= maxRating);
+
+            const filterByRating = problem => minRating <= problem.rating && problem.rating <= maxRating
+            //const filterByTags = To be implemented
+
+            const filteredProblems = problems.filter(filterByRating);
 
             if (!filteredProblems.length) {
                 throw new Error('No problems matched search.');
@@ -69,7 +123,15 @@ module.exports = {
                 )
                 .setColor(0x0099ff)
 
-            await interaction.editReply({ embeds: [embed] });
+            const completed = new ButtonBuilder()
+                .setCustomId('completed')
+                .setLabel('Completed')
+                .setStyle(ButtonStyle.Primary);
+
+            const row = new ActionRowBuilder()
+                .addComponents(completed);
+
+            await interaction.editReply({ embeds: [embed] , components: [row]});
         } catch (error) {
             console.error(error);
             await interaction.editReply('Sorry, I couldn\'t fetch a problem at this time.');
